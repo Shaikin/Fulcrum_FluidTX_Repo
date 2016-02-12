7293f6db1a1f301fa3a928119e8d40f9b3fd18fc
shaikka <sha_ikin@yahoo.co.in>
2016-02-12 17:03:31 +0530

    Included constants for workflows

---
+++ Fulcrum_FluidTX_Trunk/src/com/proj/Constants/Constants_Workflow.java
@@ -0,0 +1,21 @@
+package com.proj.Constants;
+
+public class Constants_Workflow {
+	public static final String Fulcrum_WorkFlow_Condition="Tramsmittals-TxType";
+	public static final String Fulcrum_WorkFlow_Data_Condition="TxType";
+	public static final String Fulcrim_WorkFlow_ConsultantAdvice="Consultant Advice";
+	
+	
+	
+	
+	public static final String FluidTX_WorkFlow_Condition="Tramsmittals-Issue Reason";
+	public static final String FluidTX_WorkFlow_Data_Condition="IssueReason";
+	public static final String FluidTX_WorkFlow_IssuedForApproval="Issued for Approval";
+	public static final String FluidTX_WorkFlow_IssuedForReview="Issued for Review";	
+	public static final String FluidTX_WorkFlow_IssuedForInformation="Issued for Information";
+	public static final String FluidTX_WorkFlow_RequestForInformation="Request for Information";
+	
+	
+	
+
+}
