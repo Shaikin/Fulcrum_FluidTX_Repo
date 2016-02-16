90be69fd0003de1642d7f3c0c7a92558e3459113
Shaik <sha_ikin@yahoo.co.in>
2016-02-16 18:01:41 +0530

    1. Updated Level 2 reporting. 2. Included Level1_Initaite_Transmittal_FromDocumentRegister

--- Fulcrum_FluidTX_Trunk/src/com/proj/suiteTRANSMITTALS/workflows/Workflows.java
+++ Fulcrum_FluidTX_Trunk/src/com/proj/suiteTRANSMITTALS/workflows/Workflows.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebDriver;
 import com.frw.Constants.Constants_FRMWRK;
 import com.proj.Constants.Constants_Workflow;
 import com.proj.navigations.Navigations_FluidTX;
+import com.proj.suiteDOCS.pages.DocumentRegistryPage;
 import com.proj.suiteTRANSMITTALS.TestSuiteBase;
 import com.proj.suiteTRANSMITTALS.pages.MyInboxPage_FluidTx;
 import com.proj.suiteTRANSMITTALS.pages.MySentPage_FluidTx;
@@ -51,7 +52,7 @@ public class Workflows extends TestSuiteBase{
 	public static WebDriver Level2_Validate_OR_Submit_OR_ApproveOrReject_OR_Forward_Transmittal(String siteName,WebDriver driver,String workflow_l2,String condition,String workflow_end,String url,String browsername,String username2,String password2,Hashtable<String,String>transmittalData,Hashtable<String,String>testData ) throws Throwable{
 
 		if(testData.get(Constants_Workflow.FluidTX_WorkFlow_Data_Condition).equalsIgnoreCase(Constants_Workflow.FluidTX_WorkFlow_IssuedForInformation)){
-			workflow_l2=workflow_l2+condition+" validate"+workflow_end;
+			workflow_l2=workflow_l2+condition+" & validate"+workflow_end;
 			ApplicationMethods.logOutFromApplicationAndcloseBrowser(driver);
 
 			driver=ApplicationMethods.launchBrowserAndlogIntoApplication(browsername, url, username2, password2,refID);
@@ -62,7 +63,7 @@ public class Workflows extends TestSuiteBase{
 			}
 		}
 		else if(testData.get(Constants_Workflow.FluidTX_WorkFlow_Data_Condition).equalsIgnoreCase(Constants_Workflow.FluidTX_WorkFlow_IssuedForReview)||testData.get("IssueReason").equalsIgnoreCase(Constants_Workflow.FluidTX_WorkFlow_RequestForInformation)){
-			workflow_l2=workflow_l2+condition+" Submit"+workflow_end;
+			workflow_l2=workflow_l2+condition+" & Submit"+workflow_end;
 			ApplicationMethods.logOutFromApplicationAndcloseBrowser(driver);
 
 			driver=ApplicationMethods.launchBrowserAndlogIntoApplication(browserName, url, username2, password2,refID);
@@ -80,7 +81,7 @@ public class Workflows extends TestSuiteBase{
 
 		}
 		else if(testData.get(Constants_Workflow.FluidTX_WorkFlow_Data_Condition).equalsIgnoreCase(Constants_Workflow.FluidTX_WorkFlow_IssuedForApproval) && (testData.get("Action-Level2").equals("Approved")|| testData.get("Action-Level2").equals("Rejected"))){
-			workflow_l2=workflow_l2+condition+testData.get("Action-Level2")+workflow_end;
+			workflow_l2=workflow_l2+condition+" & "+testData.get("Action-Level2")+workflow_end;
 			ApplicationMethods.logOutFromApplicationAndcloseBrowser(driver);
 
 			driver=ApplicationMethods.launchBrowserAndlogIntoApplication(browserName, url, username2, password2,refID);
@@ -98,7 +99,7 @@ public class Workflows extends TestSuiteBase{
 
 		}
 		else if((!testData.get(Constants_Workflow.FluidTX_WorkFlow_Data_Condition).equalsIgnoreCase(Constants_Workflow.FluidTX_WorkFlow_IssuedForInformation)) && (testData.get("Action-Level2").equals("Forward"))){
-			workflow_l2=workflow_l2+" "+condition+testData.get("Action-Level2")+workflow_end;
+			workflow_l2=workflow_l2+" "+condition+" & "+testData.get("Action-Level2")+workflow_end;
 			ApplicationMethods.logOutFromApplicationAndcloseBrowser(driver);
 
 			driver=ApplicationMethods.launchBrowserAndlogIntoApplication(browserName, url, username2, password2,refID);
@@ -120,7 +121,7 @@ public class Workflows extends TestSuiteBase{
 	public static WebDriver Level3_ValidateForwarded_And_ApproveOrReject_Transmittal(String siteName,WebDriver driver,String workflow_l3,String condition,String workflow_end,String url,String browsername,String username1,String password1,Hashtable<String,String>transmittalData,Hashtable<String,String>testData ) throws Throwable{
 		
 		if(!testData.get("Action-Level2").isEmpty()&& testData.get("Action-Level2").equalsIgnoreCase("Forward")){
-			workflow_l3=workflow_l3+condition+" Submit"+workflow_end;
+			workflow_l3=workflow_l3+condition+" & Submit"+workflow_end;
 			ApplicationMethods.logOutFromApplicationAndcloseBrowser(driver);
 			driver=ApplicationMethods.launchBrowserAndlogIntoApplication(browsername, url, username1, password1,refID);
 			String subj=transmittalData.get("Tramsmittals-Subject");
@@ -140,6 +141,40 @@ public class Workflows extends TestSuiteBase{
 		}
 
 		return driver;
+	}
+	
+	
+	
+	/**
+	 * Initiation of Transmittal from Document Register
+	 * @author shaikka
+	 * @param driver
+	 * @param url
+	 * @param worflow_l1
+	 * @param data
+	 * @throws Throwable
+	 */
+	public static Hashtable<String,String> Level1_Initaite_Transmittal_FromDocumentRegister(WebDriver driver,String url,String worflow_l1,Hashtable<String,String>data) throws Throwable{
+	
+		Hashtable<String,String>transmittalData=new Hashtable<String,String>();
+		DocumentRegistryPage.selectADocument(driver, worflow_l1, data);
+		DocumentRegistryPage.sendTransmittal(driver, worflow_l1);
+		Transmittals_EntryPage.attachdocument(ApplicationMethods.getSiteName(url),driver, testcaseName, refID, worflow_l1, data);
+
+		transmittalData=Transmittals_EntryPage.createAndSendTransmittalRecord(ApplicationMethods.getSiteName(url),driver, refID,testcaseName,worflow_l1,  data);
+		System.out.println("Done.."+transmittalData);
+
+		if (transmittalData.size()==0 || transmittalData.get("Tramsmittals-Subject").equals(Constants_FRMWRK.False)){
+			logsObj.log("Due to above error in the test case"+testcaseName+" hence quiting ,cannot continue further steps of the testcase");
+			CustomExceptions.Exit(testcaseName, "Failure during New Transmittal entry for the test case"+testcaseName, "Please refer the above error details for more information");
+		}
+
+		getResult=MySentPage_FluidTx.validate_TxComplete_StatusAndStatus(driver, worflow_l1, transmittalData);
+
+		if(getResult.equalsIgnoreCase(Constants_FRMWRK.False)){
+			CustomExceptions.Exit(testcaseName, worflow_l1+"- Failure", "Unable to continue the test due to above error ");
+		}
+		return transmittalData;
 	}
 
 }
