c73c5822f1db286f38367c41ae86b570212844d6
Shaik <sha_ikin@yahoo.co.in>
2016-02-17 17:00:59 +0530

    1. Change class name to MyInboxAndActionRequiredPage. 2. Included logic to accommodate My Inbox and Action Required actions.

---
+++ Fulcrum_FluidTX_Trunk/src/com/proj/suiteTRANSMITTALS/pages/MyInboxAndActionRequiredPage_FluidTx.java
@@ -0,0 +1,92 @@
+package com.proj.suiteTRANSMITTALS.pages;
+
+import java.util.Hashtable;
+
+import org.openqa.selenium.WebDriver;
+
+import com.proj.Constants.Constants_Workflow;
+import com.proj.navigations.Navigations_FluidTX;
+import com.proj.suiteTRANSMITTALS.TestSuiteBase;
+import com.proj.suiteTRANSMITTALS.reusables.TransmittalsGridUtil;
+
+public class MyInboxAndActionRequiredPage_FluidTx extends TestSuiteBase{
+	
+
+
+	/**
+	 * Validates the TxComplete Status and Status of a record for a given transmittal in My Inbox Page
+	 * @author shaikka
+	 * @param driver
+	 * @param workflow
+	 * @param returnData
+	 * @param data
+	 * @return
+	 * @throws Exception
+	 */
+	public static String validate_TxComplete_StatusAndStatus(WebDriver driver,String validationPage,String workflow,Hashtable<String,String>returnData,Hashtable<String,String>data) throws Exception{
+		String subject = null;
+		String status = null;
+		String TxComplete_Status = null;
+		
+		if(returnData.get(Constants_Workflow.FluidTX_WorkFlow_Condition).equalsIgnoreCase(Constants_Workflow.FluidTX_WorkFlow_IssuedForInformation)){
+			status="Closed";
+			TxComplete_Status="Completed";
+			subject=returnData.get("Tramsmittals-Subject");
+		}
+		else if(returnData.get(Constants_Workflow.FluidTX_WorkFlow_Condition).equalsIgnoreCase(Constants_Workflow.FluidTX_WorkFlow_IssuedForInformation)|| returnData.get(Constants_Workflow.FluidTX_WorkFlow_Condition).equalsIgnoreCase(Constants_Workflow.FluidTX_WorkFlow_IssuedForReview)||returnData.get(Constants_Workflow.FluidTX_WorkFlow_Condition).equalsIgnoreCase(Constants_Workflow.FluidTX_WorkFlow_IssuedForApproval)||returnData.get(Constants_Workflow.FluidTX_WorkFlow_Condition).equalsIgnoreCase(Constants_Workflow.FluidTX_WorkFlow_RequestForInformation)){
+			status="Open";
+			TxComplete_Status="Outstanding";
+			subject=returnData.get("Tramsmittals-Subject");			
+		}
+		if(validationPage.equalsIgnoreCase(Constants_Workflow.page_myInbox)){
+			Navigations_FluidTX.Transmittals.navigateToMyinbox(driver);
+		}else{
+			Navigations_FluidTX.Transmittals.navigateToActionRequired(driver);
+		}
+		
+		TransmittalsGridUtil.searchSubjectAndCheck_TxComplete_Status(driver,validationPage, workflow, subject, TxComplete_Status);
+		TransmittalsGridUtil.searchSubjectAndCheck_Status(driver,validationPage, workflow, subject, status);
+		if(!status.equalsIgnoreCase("Completed") && !status.equalsIgnoreCase("Closed")){
+			TransmittalsGridUtil.searchSubjectAndOpenRecord(driver,validationPage, workflow, subject);
+			Transmittals_EntryPage.waitInvisiblilityofWorkingTitle(driver);
+		}
+		return status;
+	}
+
+
+	public static String validate_TxComplete_StatusAndStatus(WebDriver driver,String validationPage,String workflow,Hashtable<String,String>data,String action) throws Exception{
+		String subject = null;
+		String status = null;
+		String TxComplete_Status = null;
+		
+		if(data.get(Constants_Workflow.FluidTX_WorkFlow_Condition).equalsIgnoreCase(Constants_Workflow.FluidTX_WorkFlow_IssuedForReview)||data.get(Constants_Workflow.FluidTX_WorkFlow_Condition).equalsIgnoreCase(Constants_Workflow.FluidTX_WorkFlow_RequestForInformation)){
+			status="Completed";
+			TxComplete_Status="Closed";
+			subject=data.get("Tramsmittals-Subject");
+		}
+		else if((data.get(Constants_Workflow.FluidTX_WorkFlow_Condition).equalsIgnoreCase(Constants_Workflow.FluidTX_WorkFlow_IssuedForApproval))&& action.equals("Approved")){
+			status="Approved";
+			TxComplete_Status="Closed";
+			subject=data.get("Tramsmittals-Subject");
+		}
+		else if((data.get(Constants_Workflow.FluidTX_WorkFlow_Condition).equalsIgnoreCase(Constants_Workflow.FluidTX_WorkFlow_IssuedForApproval))&& action.equals("Rejected")){
+			status="Rejected";
+			TxComplete_Status="Closed";
+			subject=data.get("Tramsmittals-Subject");
+		}
+		else if((!data.get(Constants_Workflow.FluidTX_WorkFlow_Condition).equalsIgnoreCase(Constants_Workflow.FluidTX_WorkFlow_IssuedForInformation)) && (action.equals("Forward"))){
+			status="Outstanding";
+			TxComplete_Status="Open";
+			subject=data.get("Tramsmittals-Subject");
+		}			
+		if(validationPage.equalsIgnoreCase(Constants_Workflow.page_myInbox)){
+			Navigations_FluidTX.Transmittals.navigateToMyinbox(driver);
+		}else{
+			Navigations_FluidTX.Transmittals.navigateToActionRequired(driver);
+		}
+		TransmittalsGridUtil.searchSubjectAndCheck_TxComplete_Status(driver,validationPage, workflow, subject, TxComplete_Status);
+		TransmittalsGridUtil.searchSubjectAndCheck_Status(driver,validationPage, workflow, subject, status);
+
+		return status;
+	}
+}
