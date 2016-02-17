f4f5b21a89c4ac49373ead405830452ba0dffa86
Shaik <sha_ikin@yahoo.co.in>
2016-02-17 16:52:16 +0530

    Included page_myInbox & page_actionRequired constants

--- Fulcrum_FluidTX_Trunk/src/com/proj/Constants/Constants_Workflow.java
+++ Fulcrum_FluidTX_Trunk/src/com/proj/Constants/Constants_Workflow.java
@@ -1,6 +1,8 @@
 package com.proj.Constants;
 
 public class Constants_Workflow {
+	public static final String page_myInbox="My Inbox";
+	public static final String page_actionRequired="Action Required";
 	public static final String Fulcrum_WorkFlow_Condition="Tramsmittals-TxType";
 	public static final String Fulcrum_WorkFlow_Data_Condition="TxType";
 	public static final String Fulcrim_WorkFlow_ConsultantAdvice="Consultant Advice";
