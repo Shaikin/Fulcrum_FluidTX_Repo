9db06efce59074e6216fdd00fd19d2c320549669
Shaik <sha_ikin@yahoo.co.in>
2016-02-17 17:04:41 +0530

    1. Changed deFocusCursor to shift+tab 2. Increased the pause between f12

--- Fulcrum_FluidTX_Trunk/src/com/proj/utilFulcrum/WorkArounds.java
+++ Fulcrum_FluidTX_Trunk/src/com/proj/utilFulcrum/WorkArounds.java
@@ -12,7 +12,10 @@ import com.proj.library.commonMethods;
 public class WorkArounds {
 
 	public static void deFocusCursor(WebDriver driver){
-		driver.findElement(By.tagName("body")).click();
+		Actions action =new Actions (driver);
+		action.keyDown(Keys.SHIFT).sendKeys(Keys.TAB).build().perform();
+		action.keyUp(Keys.SHIFT).build().perform();
+		
 		WaitUtil.pause(Constants_TimeOuts.generic_TimeOut);
 
 		System.out.println("Defocussed....");
@@ -22,16 +25,14 @@ public class WorkArounds {
 		commonMethods.switchToDefaultPage(driver);
 		WaitUtil.pause(2);
 		driver.findElement(By.tagName("body")).sendKeys(Keys.F12);
-		WaitUtil.pause(1);
+		WaitUtil.pause(Constants_TimeOuts.Save_TimeOut);
 		driver.findElement(By.tagName("body")).sendKeys(Keys.F12);
 	}
 
 
 	public static void deFocusCursor_shiftAndTab(WebDriver driver){
 
-		Actions action =new Actions (driver);
-		action.keyDown(Keys.SHIFT).sendKeys(Keys.TAB).build().perform();
-		action.keyUp(Keys.SHIFT).build().perform();
+		driver.findElement(By.tagName("body")).click();
 		System.out.println("Defocussed....");
 	}
 	public static void deFocusCursor_old(WebDriver driver){
