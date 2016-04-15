6e9715c5690eeb0b2d183d21df871d8536a7eef7
Anupama <anupama.thumrugoti@gmail.com>
2016-04-15 19:08:55 +0530

    Replaced Config with Constants_configProperties username,password, testSiteName

--- Fulcrum_FluidTX_Trunk/src/com/proj/suiteDOCS/FLD_DocumentRegistry_New_Transmittals_Subsite.java
+++ Fulcrum_FluidTX_Trunk/src/com/proj/suiteDOCS/FLD_DocumentRegistry_New_Transmittals_Subsite.java
@@ -10,6 +10,7 @@ import org.testng.annotations.Test;
 
 import com.frw.Constants.Constants_FRMWRK;
 import com.proj.Constants.Constants;
+import com.proj.Constants.Constants_ConfigProperties;
 import com.proj.Constants.Constants_Workflow;
 import com.proj.library.LocalDriverManager;
 import com.proj.suiteTRANSMITTALS.workflows.Workflows;
@@ -27,9 +28,9 @@ public class FLD_DocumentRegistry_New_Transmittals_Subsite extends TestSuiteBase
 	private static String workflow_l1="Level-1:-Initiation of Transmittal From Document Register to Subsite";
 	private static String workflow_l2="Level-2:-Recieve Transmittal with document attached from Document Register to Subsite";
 	private static String workflow_end=" || ";
-	private static String url;
-	private static String username1;
-	private static String password1;
+//	private static String url;
+//	private static String username1;
+//	private static String password1;
 
 	private static boolean isBeforeTestPass=Constants_FRMWRK.TrueB;
 	private static boolean isBeforeMethodPass=Constants_FRMWRK.TrueB;
@@ -42,9 +43,9 @@ public class FLD_DocumentRegistry_New_Transmittals_Subsite extends TestSuiteBase
 		testcaseName=TestExecutionUtil.getTestcaseName(local_testcaseName);		
 		scenarioName=testcaseName;
 		moduleName=Constants.Module_DOCS;
-		url=CONFIG.getProperty("testSiteName");
-		username1=CONFIG.getProperty("userUserName");
-		password1=CONFIG.getProperty("userpassword");
+//		url=CONFIG.getProperty("testSiteName");
+//		username1=CONFIG.getProperty("userUserName");
+//		password1=CONFIG.getProperty("userpassword");
 		try{
 			TestExecutionUtil.initialiseTestFlags(testcaseName);
 		}catch (Throwable t){
@@ -57,7 +58,7 @@ public class FLD_DocumentRegistry_New_Transmittals_Subsite extends TestSuiteBase
 	public static void befMethod() throws Throwable{
 		try {
 			if(!isBeforeTestPass ==Constants_FRMWRK.FalseB){				
-				driver_DOCS=ApplicationMethods.launchBrowserAndlogIntoApplication(browserName, url, username1, password1, refID);
+				driver_DOCS=ApplicationMethods.launchBrowserAndlogIntoApplication(browserName, Constants_ConfigProperties.testSiteName, Constants_ConfigProperties.username_SuperUser, Constants_ConfigProperties.password_SuperUser, refID);
 				logsObj.log("Before method success for "+testcaseName);
 			}else{
 				CustomExceptions.Exit(testcaseName, "Before Method-Failure", "Due to above error in the Before Test cannot execute the test..");
@@ -84,18 +85,18 @@ public class FLD_DocumentRegistry_New_Transmittals_Subsite extends TestSuiteBase
 			CustomExceptions.Exit(testcaseName, "Before Method-Failure", "Due to above error in the Before Method cannot execute the test..");
 		}
 
-		String siteName=ApplicationMethods.getSiteName(url);
+		String siteName=ApplicationMethods.getSiteName(Constants_ConfigProperties.testSiteName);
 		String condition="";
 		condition=" ["+data.get("IssueReason")+"]";
 
 		//************************************** LEVEL 1 *****************************************************************************
 		String workflow_lvl1=workflow_l1+condition+workflow_end;		
 
-		transmittalData=Workflows.Level1_Initaite_Transmittal_FromDocumentRegister(driver_DOCS, url, workflow_lvl1, data);
+		transmittalData=Workflows.Level1_Initaite_Transmittal_FromDocumentRegister(driver_DOCS, Constants_ConfigProperties.testSiteName, workflow_lvl1, data);
 		//************************************** LEVEL 2 *****************************************************************************
-		String subsite=ApplicationMethods.getSubsite(url, data.get("Subsite"));
-		String username=CONFIG.getProperty("userUserName3");
-		String password=CONFIG.getProperty("userpassword3");
+		String subsite=ApplicationMethods.getSubsite(Constants_ConfigProperties.testSiteName, data.get("Subsite"));
+		String username=Constants_ConfigProperties.username_SubSiteUser;
+		String password=Constants_ConfigProperties.password_SubSiteUser;
 		driver_DOCS=Workflows.Level2_Validate_OR_Submit_OR_ApproveOrReject_OR_Forward_OR_ReplyAll_Transmittal(siteName,Constants_Workflow.page_myInbox,driver_DOCS,refID,testcaseName, workflow_l2, condition, workflow_end, subsite, browserName, username, password, transmittalData, data,1);
 
 		logsObj.log(" after test of "+testcaseName+"-testresult"+isTestPass);
