98435d4d31b8b95b3e6486476c3680d3b842b02b
shaikka <sha_ikin@yahoo.co.in>
2016-04-24 12:07:45 +0530

    Cleared befTest,AftTest,BefMethod,AftMethod from the test class.

--- Fulcrum_FluidTX_Trunk/src/com/proj/suiteDOCS/FLD_DocumentRegistry_New_Transmittals.java
+++ Fulcrum_FluidTX_Trunk/src/com/proj/suiteDOCS/FLD_DocumentRegistry_New_Transmittals.java
@@ -2,17 +2,11 @@ package com.proj.suiteDOCS;
 
 import java.util.Hashtable;
 
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import com.frw.Constants.Constants_FRMWRK;
-import com.proj.Constants.Constants;
 import com.proj.Constants.Constants_ConfigProperties;
 import com.proj.Constants.Constants_Workflow;
-import com.proj.library.LocalDriverManager;
 import com.proj.suiteTRANSMITTALS.pages.Transmittals_EntryPage;
 import com.proj.suiteTRANSMITTALS.workflows.Workflows;
 import com.proj.util.CustomExceptions;
@@ -21,62 +15,10 @@ import com.proj.utilFulcrum.ApplicationMethods;
 
 public class FLD_DocumentRegistry_New_Transmittals extends TestSuiteBase{
 
-	private String local_testcaseName=FLD_DocumentRegistry_New_Transmittals.class.getSimpleName();
-	private String local_refID="LATFLD-13";
-	String input="";
-	static String getResult="";
 	static Hashtable<String,String>transmittalData=new Hashtable<String,String>();	
 	private static String workflow_l1="Level-1:-Initiation of Transmittal From Document Register ";
 	private static String workflow_l2="Level-2:-Recieve Transmittal with document attached from Document Register ";
 	private static String workflow_end=" || ";
-	//	private static String url;
-	//	private static String username1;
-	//	private static String password1;
-
-	private static boolean isBeforeTestPass=Constants_FRMWRK.TrueB;
-	private static boolean isBeforeMethodPass=Constants_FRMWRK.TrueB;
-
-	@BeforeTest
-	public void testPrerequisite() throws Throwable{
-		refID=local_refID;
-		logsObj.log("starting before test of "+local_testcaseName);
-		isTestPass=Constants_FRMWRK.TrueB;
-		testcaseName=TestExecutionUtil.getTestcaseName(local_testcaseName);		
-		scenarioName=testcaseName;
-		moduleName=Constants.Module_DOCS;
-		//		url=CONFIG.getProperty("testSiteName");
-		//		username1=CONFIG.getProperty("userUserName");
-		//		password1=CONFIG.getProperty("userpassword");
-		try{
-			TestExecutionUtil.initialiseTestFlags(testcaseName);
-		}catch (Throwable t){
-			isBeforeTestPass=Constants_FRMWRK.FalseB;
-		}
-		logsObj.log(" after test of "+testcaseName);
-	}
-
-	@BeforeMethod
-	public static void befMethod() throws Throwable{
-		try {
-			if(!isBeforeTestPass ==Constants_FRMWRK.FalseB){				
-				driver_DOCS=ApplicationMethods.launchBrowserAndlogIntoApplication(browserName, Constants_ConfigProperties.testSiteName, Constants_ConfigProperties.username_SuperUser, Constants_ConfigProperties.password_SuperUser, refID);
-				logsObj.log("Before method success for "+testcaseName);
-			}else{
-				CustomExceptions.Exit(testcaseName, "Before Method-Failure", "Due to above error in the Before Test cannot execute the test..");
-			}	
-
-		} catch (Throwable t) {
-			isTestPass=Constants_FRMWRK.FalseB;
-			logsObj.log("Not able to login into the application due to error "+t+" hence cannot continue execution of "+testcaseName);
-			try{
-				CustomExceptions.Exit(testcaseName, "Log into the application", "Not able to login into the application due to error "+t+" hence cannot continue execution of "+testcaseName);
-			} catch (Exception e) {
-				isTestPass=Constants_FRMWRK.FalseB;
-				isBeforeMethodPass=Constants_FRMWRK.FalseB;
-			}
-		}
-	}
-
 
 	@Test(dataProviderClass=com.proj.util.DataProviders.class,dataProvider="getData_Global")
 	public static void TestFLD_DocumentRegistry_New_Transmittals(Hashtable<String,String>data
@@ -86,7 +28,7 @@ public class FLD_DocumentRegistry_New_Transmittals extends TestSuiteBase{
 		logsObj.log("Executing the test case: "+testcaseName);
 
 		try{
-			if(isBeforeMethodPass==Constants_FRMWRK.FalseB){
+			if(isBeforeMethodPass_docs==Constants_FRMWRK.FalseB){
 				CustomExceptions.Exit(testcaseName, "Before Method-Failure", "Due to above error in the Before Method cannot execute the test..");
 			}
 			String siteName=ApplicationMethods.getSiteName(Constants_ConfigProperties.testSiteName);
@@ -126,25 +68,5 @@ public class FLD_DocumentRegistry_New_Transmittals extends TestSuiteBase{
 
 		TestExecutionUtil.AssertTestStatus(isTestPass);
 	}
-
-
-	@AfterMethod
-	public static void aftMethod(){
-		logsObj.log("Aft method "+testcaseName);
-		try {
-			if (!isBeforeMethodPass==Constants_FRMWRK.FalseB){
-				ApplicationMethods.logOutFromApplicationAndcloseBrowser(LocalDriverManager.getDriver(),refID,testcaseName);
-				logsObj.log(" after test of "+testcaseName+"-AfterTest successful");			}
-		} catch (Throwable t) {
-			logsObj.log("Not able to logout to the application due to error "+t+" hence cannot continue execution of "+testcaseName);
-
-		}
-	}
-
-	@AfterTest
-	public void Report_TestResult() throws Throwable{
-		TestExecutionUtil.resultTest(isTestPass,suiteDOCSxls, testcaseName);
-	}
-
 
 }
--- Fulcrum_FluidTX_Trunk/src/com/proj/suiteDOCS/FLD_DocumentRegistry_New_Transmittals_Subsite.java
+++ Fulcrum_FluidTX_Trunk/src/com/proj/suiteDOCS/FLD_DocumentRegistry_New_Transmittals_Subsite.java
@@ -2,17 +2,11 @@ package com.proj.suiteDOCS;
 
 import java.util.Hashtable;
 
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import com.frw.Constants.Constants_FRMWRK;
-import com.proj.Constants.Constants;
 import com.proj.Constants.Constants_ConfigProperties;
 import com.proj.Constants.Constants_Workflow;
-import com.proj.library.LocalDriverManager;
 import com.proj.suiteTRANSMITTALS.workflows.Workflows;
 import com.proj.util.CustomExceptions;
 import com.proj.util.TestExecutionUtil;
@@ -20,61 +14,11 @@ import com.proj.utilFulcrum.ApplicationMethods;
 
 public class FLD_DocumentRegistry_New_Transmittals_Subsite extends TestSuiteBase{
 
-	private String local_testcaseName=FLD_DocumentRegistry_New_Transmittals_Subsite.class.getSimpleName();
-	private String local_refID="LATFLD-14";
-	String input="";
-	static String getResult="";
+
 	static Hashtable<String,String>transmittalData=new Hashtable<String,String>();	
 	private static String workflow_l1="Level-1:-Initiation of Transmittal From Document Register to Subsite";
 	private static String workflow_l2="Level-2:-Recieve Transmittal with document attached from Document Register to Subsite";
 	private static String workflow_end=" || ";
-	//	private static String url;
-	//	private static String username1;
-	//	private static String password1;
-
-	private static boolean isBeforeTestPass=Constants_FRMWRK.TrueB;
-	private static boolean isBeforeMethodPass=Constants_FRMWRK.TrueB;
-
-	@BeforeTest
-	public void testPrerequisite() throws Throwable{
-		refID=local_refID;
-		logsObj.log("starting before test of "+local_testcaseName);
-		isTestPass=Constants_FRMWRK.TrueB;
-		testcaseName=TestExecutionUtil.getTestcaseName(local_testcaseName);		
-		scenarioName=testcaseName;
-		moduleName=Constants.Module_DOCS;
-		//		url=CONFIG.getProperty("testSiteName");
-		//		username1=CONFIG.getProperty("userUserName");
-		//		password1=CONFIG.getProperty("userpassword");
-		try{
-			TestExecutionUtil.initialiseTestFlags(testcaseName);
-		}catch (Throwable t){
-			isBeforeTestPass=Constants_FRMWRK.FalseB;
-		}
-		logsObj.log(" after test of "+testcaseName);
-	}
-
-	@BeforeMethod
-	public static void befMethod() throws Throwable{
-		try {
-			if(!isBeforeTestPass ==Constants_FRMWRK.FalseB){				
-				driver_DOCS=ApplicationMethods.launchBrowserAndlogIntoApplication(browserName, Constants_ConfigProperties.testSiteName, Constants_ConfigProperties.username_SuperUser, Constants_ConfigProperties.password_SuperUser, refID);
-				logsObj.log("Before method success for "+testcaseName);
-			}else{
-				CustomExceptions.Exit(testcaseName, "Before Method-Failure", "Due to above error in the Before Test cannot execute the test..");
-			}	
-
-		} catch (Throwable t) {
-			isTestPass=Constants_FRMWRK.FalseB;
-			logsObj.log("Not able to login into the application due to error "+t+" hence cannot continue execution of "+testcaseName);
-			try{
-				CustomExceptions.Exit(testcaseName, "Log into the application", "Not able to login into the application due to error "+t+" hence cannot continue execution of "+testcaseName);
-			} catch (Exception e) {
-				isTestPass=Constants_FRMWRK.FalseB;
-				isBeforeMethodPass=Constants_FRMWRK.FalseB;
-			}
-		}
-	}
 
 
 	@Test(dataProviderClass=com.proj.util.DataProviders.class,dataProvider="getData_Global")
@@ -85,7 +29,7 @@ public class FLD_DocumentRegistry_New_Transmittals_Subsite extends TestSuiteBase
 		logsObj.log("Executing the test case: "+testcaseName);
 
 		try{
-			if(isBeforeMethodPass==Constants_FRMWRK.FalseB){
+			if(isBeforeMethodPass_docs==Constants_FRMWRK.FalseB){
 				CustomExceptions.Exit(testcaseName, "Before Method-Failure", "Due to above error in the Before Method cannot execute the test..");
 			}
 
@@ -110,25 +54,4 @@ public class FLD_DocumentRegistry_New_Transmittals_Subsite extends TestSuiteBase
 
 		TestExecutionUtil.AssertTestStatus(isTestPass);
 	}
-
-
-	@AfterMethod
-	public static void aftMethod(){
-		logsObj.log("Aft method "+testcaseName);
-		try {
-			if (!isBeforeMethodPass==Constants_FRMWRK.FalseB){
-				ApplicationMethods.logOutFromApplicationAndcloseBrowser(LocalDriverManager.getDriver(),refID,testcaseName);
-				logsObj.log(" after test of "+testcaseName+"-AfterTest successful");			}
-		} catch (Throwable t) {
-			logsObj.log("Not able to logout to the application due to error "+t+" hence cannot continue execution of "+testcaseName);
-
-		}
-	}
-
-	@AfterTest
-	public void Report_TestResult() throws Throwable{
-		TestExecutionUtil.resultTest(isTestPass,suiteDOCSxls, testcaseName);
-	}
-
-
 }
