1f7816ed597c1d2935d2c03213904c598e6f3238
Shaik <sha_ikin@yahoo.co.in>
2016-05-20 18:15:03 +0530

    Cleared unused imports from test class

--- Fulcrum_FluidTX_Trunk/src/com/proj/suiteDOCS/FLD_CreateNewDocument.java
+++ Fulcrum_FluidTX_Trunk/src/com/proj/suiteDOCS/FLD_CreateNewDocument.java
@@ -2,18 +2,15 @@ package com.proj.suiteDOCS;
 
 import java.util.Hashtable;
 
-import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 
 import com.frw.Constants.Constants_FRMWRK;
-import com.proj.Constants.Constants_ConfigProperties;
 import com.proj.library.LocalDriverManager;
 import com.proj.navigations.Navigations_FluidTX;
 import com.proj.suiteDOCS.pages.Documents_EntryPage;
 import com.proj.suiteDOCS.reusables.DocumentRegisterGridUtil;
 import com.proj.util.CustomExceptions;
 import com.proj.util.TestExecutionUtil;
-import com.proj.utilFulcrum.ApplicationMethods;
 
 public class FLD_CreateNewDocument extends TestSuiteBase{
 	private static String workflow_UploadDocument="Uploading a file";
@@ -39,7 +36,7 @@ public class FLD_CreateNewDocument extends TestSuiteBase{
 				if(isBeforeMethodPass_docs==Constants_FRMWRK.FalseB){
 					CustomExceptions.Exit(testcaseName, "Before Method-Failure", "Due to above error in the Before Method cannot execute the test..");
 				}
-				String siteName=ApplicationMethods.getSiteName(Constants_ConfigProperties.testSiteName);
+				
 				String condition="";
 				
 				Navigations_FluidTX.navigateToDocuments(LocalDriverManager.getDriver());
